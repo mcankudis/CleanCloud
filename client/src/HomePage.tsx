089a55408a9a15584187b515892154b63eb94bf8
Teoman Nejat Wiedenbeck <t.wiedenbeck.1@campus.tu-berlin.de>
2023-05-30 16:20:24 +0200

    feat: add Example Table

--- client/src/HomePage.tsx
+++ client/src/HomePage.tsx
@@ -5,12 +5,37 @@ function HomePage() {
 
   return (
     <>
-      <h1>All your Cloudserver</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>Your Cloudserver List</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Standort</th>
+            <th scope="col">Name</th>
+            <th scope="col">CO2-Intenzität</th>
+            <th scope="col">Provider</th>
+          </tr>
+        </thead>
+        <tbody className="table-group-divider">
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td >Larry the Bird</td>
+            <td >wat</td>
+            <td >@twitter</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 }
