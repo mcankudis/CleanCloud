d48d76db94b06319de749739428dee936e955cc0
Teoman Nejat Wiedenbeck <t.wiedenbeck.1@campus.tu-berlin.de>
2023-06-10 13:39:38 +0200

    feat: Names in About no link to our GitHub-acc

--- client/src/about/AboutView.tsx
+++ client/src/about/AboutView.tsx
@@ -3,8 +3,8 @@ export const AboutView = () => {
         <div className="container mt-4">
             <div className="flex w-screen flex-col items-center justify-center mr-4">
                 <h1>About</h1>
-                <p>Teoman Wiedenbeck</p>
-                <p>Mikolaj Cankudis</p>
+                <a href="https://github.com/TeoDevGerman">Teoman Wiedenbeck</a>
+                <a href="https://github.com/mcankudis">Mikolaj Cankudis</a>
                 <p>...</p>
                 <p>...</p>
             </div>
