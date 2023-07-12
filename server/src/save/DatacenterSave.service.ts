d804aa8328944ff33f8b383c8bcc1439efa73dd2
Miko Cankudis <mikolaj@cankudis.net>
2023-07-12 15:06:04 +0200

    fix: saving breaks datacenter estimate display

--- server/src/save/DatacenterSave.service.ts
+++ server/src/save/DatacenterSave.service.ts
@@ -35,34 +35,7 @@ export class DatacenterSaveService {
 
         if (!save) throw new HttpException('Save not found', HttpStatus.NOT_FOUND);
 
-        const promises: Promise<DatacenterSavePositionResponse>[] = save.positions.map(
-            async (position) => {
-                let estimate;
-                try {
-                    estimate = await this.estimateService
-                        .getEstimate(position.latitude, position.longitude)
-                        .catch();
-                } catch (error) {
-                    this.Logger.error(
-                        `Error fetching estimate for (${position.latitude}, ${position.longitude})`,
-                        error
-                    );
-                    estimate = undefined;
-                }
-
-                return {
-                    latitude: position.latitude,
-                    longitude: position.longitude,
-                    name: position.name,
-                    baseEstimate: estimate,
-                    projectedEnergyConsumptionInKWh: position.projectedEnergyConsumptionInKWh,
-                };
-            }
-        );
-
-        this.Logger.debug('Enriched postions with estimates');
-
-        const positions = await Promise.all(promises);
+        const positions = await this.addEstimatesToPositions(save.positions);
 
         if (save) {
             this.Logger.debug(`Updating lastAccess on save ${id}`);
@@ -91,7 +64,9 @@ export class DatacenterSaveService {
 
         this.Logger.debug(`Created save`, res);
 
-        return res;
+        const positions = await this.addEstimatesToPositions(res.positions);
+
+        return { id: res.id, lastAccess: res.lastAccess, positions };
     }
 
     public async updateDatacenterSave(update: DatacenterSaveUpdateRequest) {
@@ -106,7 +81,40 @@ export class DatacenterSaveService {
 
         this.Logger.debug('Updated DatacenterSave', res);
 
-        return res;
+        const positions = await this.addEstimatesToPositions(res.positions);
+
+        return { id: res.id, lastAccess: res.lastAccess, positions };
+    }
+
+    private async addEstimatesToPositions(positions: DatacenterSavePositionResponse[]) {
+        const promises: Promise<DatacenterSavePositionResponse>[] = positions.map(
+            async (position) => {
+                let estimate;
+                try {
+                    estimate = await this.estimateService
+                        .getEstimate(position.latitude, position.longitude)
+                        .catch();
+                } catch (error) {
+                    this.Logger.error(
+                        `Error fetching estimate for (${position.latitude}, ${position.longitude})`,
+                        error
+                    );
+                    estimate = undefined;
+                }
+
+                return {
+                    latitude: position.latitude,
+                    longitude: position.longitude,
+                    name: position.name,
+                    baseEstimate: estimate,
+                    projectedEnergyConsumptionInKWh: position.projectedEnergyConsumptionInKWh,
+                };
+            }
+        );
+
+        this.Logger.debug('Enriched postions with estimates');
+
+        return await Promise.all(promises);
     }
 
     @Cron(CronExpression.EVERY_DAY_AT_NOON)
