9c90daf5332fba36fb58db8f074998d23d189771
Miko Cankudis <mikolaj@cankudis.net>
2023-07-12 15:10:41 +0200

    feat: update logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
                 <div className="mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-16">
                         <Link to="/" className="flex-shrink-0">
-                            <img className="max-h-16" src="/logo.png" alt="Clean Cloud Logo" />
+                            <img className="max-h-16" src="/banner.png" alt="Clean Cloud Logo" />
                         </Link>
                         <div className="hidden md:block">
                             <div className="ml-10 flex items-center space-x-4">
