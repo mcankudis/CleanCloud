26b6096ee81a363ccc516f7543f7a079a1fdea32
Miko Cankudis <mikolaj@cankudis.net>
2023-07-21 21:22:28 +0200

    feat: improve about text

--- client/src/about/AboutView.tsx
+++ client/src/about/AboutView.tsx
@@ -48,55 +48,57 @@ export const AboutView = () => {
     }, []);
 
     return (
-        <div>
-            <h1>About</h1>
-            <div className="flex justify-center">
-                <p>
-                    <br></br>
-                    <br></br>
-                    Driven by a rapid growth of digital technologies and an increasing demand in availability, <br></br>
-                    the world of computing and network infrastructure is in the midst of a profound transformation
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    In the next decade, as existing and new businesses find themselves required to meet client demands of global <br></br>
-                    access to digital products and services, the need for efficient and sustainable solutions for  <br></br>
-                    data processing and storage will soar.
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    CleanClouds mission is to over existing industry entities and newcomers a product which helps make mindful and efficient decisions <br></br> 
-                    when choosing locations for compute- and network-infrastructure.
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    With our CleanCloud, you can easily specify locations and 
-                    immediately get information about the CO2 emissions of <br></br>
-                    electricity consumption as well as the electricity price of this
-                    region. You can also customize the size of <br></br>
-                    your data center and specify the expected power 
-                    consumption. Our platform gives you a comprehensive <br></br>
-                    overview so you can make informed decisions and build a 
-                    sustainable data center. Save money, minimize your carbon<br></br>
-                    footprint, and increase the efficiency of your business. With 
-                    Clean Cloud, you'll find the perfect location for your data <br></br>
-                    center with clean energy at its core!
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    CleanCloud was created by a group of students from Berlin University of Technology in the scope of a course project. <br></br>
-                    The goal was to create a 'dashboard' for accessing data related to carbon intesity of electricity production and <br></br>
-                    build a real-world use case around it. All data is taken from the electricitymaps API and energy prices are calculated <br></br>
-                    based on hand picked static data found online.
-                    <br></br>
-                    <br></br>
-                    
+        <div className="container m-auto max-w-2xl">
+            <h1 className="mt-6">About</h1>
 
-                </p>
-            </div>
-            <h1>Meet the Team</h1>
-            <div className="flex justify-center">
+            <p className="mt-1 text-justify">
+                Driven by rapid growth of digital technologies and increasing demand in
+                availability, the world of computing and network infrastructure is in the midst of a
+                profound transformation.
+            </p>
+            <p className="mt-10 text-justify">
+                In the next decade, as existing and new businesses find themselves required to meet
+                client demands of global access to digital products and services, the need for
+                efficient and sustainable solutions for data processing and storage will soar.
+            </p>
+            <p className="mt-10 text-justify">
+                CleanCloud's mission is to offer existing industry entities and newcomers a product
+                facilitating making mindful and efficient decisions when choosing locations for
+                compute- and network infrastructure.
+            </p>
+            <p className="mt-10 text-justify">
+                You can easily add locations by clicking on our map, and immediately get information
+                about the CO<sub>2</sub> emissions caused by electricity consumption, as well as the
+                average electricity price for this region. You can also customize the size of your
+                data center and specify a time frame in order to be shown total estimated energy
+                consumption and CO<sub>2</sub> emissions.
+            </p>
+            <p className="mt-10 text-justify">
+                Our platform gives you a comprehensive overview so you can make informed decisions
+                and build a sustainable data center. Save money, minimize your carbon footprint, and
+                increase the efficiency of your business.
+            </p>
+            <p className="mt-6 text-justify">
+                <b>
+                    With CleanCloud, you'll find the perfect location for your data center with
+                    clean energy at its core!
+                </b>
+            </p>
+
+            <p className="mt-6 text-justify">
+                CleanCloud was created by a group of students from Berlin University of Technology
+                as a project assignment in a course. The goal was to create a 'dashboard' for
+                accessing data related to carbon intesity of electricity production and build a
+                real-world use case around it, focusing on software engineering processes, such as
+                working in an agile way with modern technologies, Git, and DevOps. All CO
+                <sub>2</sub>-related data is sourced from{' '}
+                <a href="https://www.electricitymaps.com/">Electricitymaps</a> and energy prices are
+                calculated based on hand picked statistical data found online.
+            </p>
+
+            <h1 className="mt-10">Meet the Team</h1>
+
+            <div className="flex justify-center mb-10">
                 <div className="row">
                     {members.map((member) => (
                         <div
